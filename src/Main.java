65a92b49dcd45b403da7c8e46a3bf75d1ebaa00e
KI0T0 <f_g@outlook.com.br>
2024-06-28 09:32:11 -0300

    Criando classe pessoa e métodos

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,20 @@
 public class Main {
     public static void main(String[] args) {
 
+//1º exercício - Classe Pessoa
+
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa("João",28,1.90,83);
+        p1.setIdade(30);
+        p1.envelhecer(5);
+
+        p2.envelhecer(2);
+        System.out.println(p1.getIdade());
+        System.out.println(p2.getIdade());
+
+        p1.engordar(10);
+        p2.engordar(5);
+        System.out.println(p1.getPeso()+"\n"+p2.getPeso());
+
         }
     }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,59 @@
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+    private double peso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(String nome, int idade, double altura, double peso) {
+        setNome(nome);
+        setIdade(idade);
+        setAltura(altura);
+        setPeso(peso);
+    }
+
+    public void envelhecer(int ano) {
+        setIdade(idade+ano);
+    }
+
+    public void engordar(double massa) {
+        setPeso(peso+massa);
+    }
 
 }
