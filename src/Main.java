734ac1e0703e19395bfa9e8f91458b5d57f3a3f1
KI0T0 <f_g@outlook.com.br>
2024-07-01 18:09:26 -0300

    6 exercícios prontos...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -20,10 +18,84 @@ public class Main {
 //        p2.emagrecer(20);
 //        p2.mostrarInformacoes();
 
-//      2º exercício - Classe Produto
+//2º exercício - Classe Produto
+//          Produto p1 = new Produto("Coca-Cola", 5.00, 20);
+//
+//          p1.atualizarEstoque(25);
+//          p1.exibirDetalhes();
+//
+//
+//          Produto p2 = new Produto();
+//
+//          p2.setNome("Dolly");
+//          p2.setPreco(2.50);
+//          p2.setQtdEmEstoque(15);
+//
+//          p2.atualizarEstoque(10);
+//          p2.exibirDetalhes();
+//
+//          p2.comprar(3);
+
+//3º exercício - Classe Filme
+
+//        Filme filme1 = new Filme("O Poderoso Chefão III", "Francis Ford Coppola", 1990, "Crime, Drama");
+//        Filme filme2 = new Filme("Pantera Negra", "Ryan Coogler", 2018, "Ação, Aventura, Ficção Científica");
+//
+//
+//        filme1.mostrarDetalhes();
+//        filme2.mostrarDetalhes();
+//
+//        ArrayList<Filme> listaFilmes = new ArrayList<>();
+//        listaFilmes.add(filme1);
+//        listaFilmes.add(filme2);
+//
+//        System.out.println("Lista de Filmes:");
+//        for (Filme filme : listaFilmes) {
+//            filme.mostrarDetalhes();
+//        }
+
+//4º exercício - Classe Funcionário
+
+//        Funcionario funcionario1 = new Funcionario("Felipe Gonçalves", "Desenvolvedor", 4000.00, "TI");
+//
+//        System.out.println("Detalhes do profissional:");
+//        funcionario1.exibirDetalhes();
+//
+//        funcionario1.promover("Gerente", 8000.00);
+//
+//        System.out.println("Cargo atualizado após a promoção:");
+//        funcionario1.exibirDetalhes();
+//
+
+//5º exercício - Classe Casa
+
+//        Casa casa = new Casa("Rua João Batista Duarte, 49", 4, 150.5, 300000.00);
+//
+//        System.out.println("Detalhes da Casa:");
+//        casa.exibirDetalhes();
+//
+//        double taxaImposto = 1.5;
+//        double valorImposto = casa.valorImposto(taxaImposto);
+//        System.out.printf("Valor do Imposto (taxa %.2f%%): R$ %.2f\n", taxaImposto, valorImposto);
+
+
+//6º exercício - Classe Celular
+
+        Celular meuCelular = new Celular("Apple", "Iphone 15 Pro", 128);
+
+        System.out.println("Informações do celular:");
+        meuCelular.mostraInformacoes();
+
+        meuCelular.instalarApp("WhatsApp");
+        meuCelular.instalarApp("Instagram");
+        meuCelular.instalarApp("Duolingo");
+
+        System.out.println("Celular após a instalação dos apps:");
+        meuCelular.mostraInformacoes();
+
+
+    }
 
 
 
-
-        }
     }
--- src/Produto.java
+++ src/Produto.java
@@ -1,3 +1,61 @@
 public class Produto {
+    protected String nome;
+    protected double preco;
+    protected int qtdEmEstoque;
+
+    public Produto(){}
+
+    public Produto(String nome, double preco, int qtdEmEstoque) {
+        setNome(nome);
+        setPreco(preco);
+        setQtdEmEstoque(qtdEmEstoque);
+    }
+
+    public void atualizarEstoque(int qtdNova){
+        setQtdEmEstoque(qtdNova);
+    }
+
+    public void exibirDetalhes(){
+        System.out.printf("Nome do produto: "+getNome()+"\nPreço: %.2f\nQuantidade: "+getQtdEmEstoque()+
+                "\n---------------------\n",getPreco());
+    }
+
+    public void comprar(int quantidade) {
+        if (quantidade > 0 && quantidade <= qtdEmEstoque) {
+            qtdEmEstoque -= quantidade;
+            System.out.printf("Compra realizada com sucesso! Comprou %d unidades de %s.\n", quantidade, nome);
+        } else {
+            System.out.println("Estoque insuficiente para realizar a compra.");
+        }
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdEmEstoque() {
+        return qtdEmEstoque;
+    }
+
+    public void setQtdEmEstoque(int qtdEmEstoque) {
+        this.qtdEmEstoque = qtdEmEstoque;
+    }
+
+
+
+
 
 }
